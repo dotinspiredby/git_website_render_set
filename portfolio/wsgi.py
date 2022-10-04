b4fbbd1b127e404b6d194f03fb3080a69b6d8b67
Katya Meldre <imkacello@gmail.com>
2022-10-04 03:50:11 +0300

    automatic su

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+import environ
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth.models import User
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
 
 application = get_wsgi_application()
+
+users = User.objects.all()
+if not users:
+    User.objects.create_superuser(username=env("ADMIN"), email=env("ADMIN_EMAIL"),
+                                  password=env("ADMIN_PASSWORD"), is_active=True, is_staff=True)
